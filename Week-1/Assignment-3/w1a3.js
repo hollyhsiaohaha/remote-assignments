c1d4e8de4909e29088edffa4239d97f5ed18ab50
holly <yahanhsiao@gmail.com>
2023-09-02 14:40:15 +0800

    adjust some format of js

--- Week-1/Assignment-3/w1a3.js
+++ Week-1/Assignment-3/w1a3.js
@@ -1,18 +1,18 @@
 function countAandB(input) {
-	let count = 0;
-	input.forEach(element => {
-		if (element === 'a' || element === 'b') {
-			count++;
-		}
-	});
-	return (count)
+  let count = 0;
+  input.forEach(element => {
+    if (element === 'a' || element === 'b') {
+      count++;
+    }
+  });
+  return count;
 }
 function toNumber(input) {
-	const res_list = [];
-	input.forEach(element => {
-		res_list.push(element.charCodeAt() - 'a'.charCodeAt() + 1)
-	});
-	return (res_list)
+  const res_list = [];
+  input.forEach(element => {
+    res_list.push(element.charCodeAt() - 'a'.charCodeAt() + 1)
+  });
+  return res_list;
 }
 
 let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
