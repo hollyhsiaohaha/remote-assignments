addb243cf993dcab577d9aff3369acacf11842a8
holly <yahanhsiao@gmail.com>
2023-09-04 16:26:43 +0800

    ++ w2a5

--- Week-2/Assignment-5/w2a5.js
+++ Week-2/Assignment-5/w2a5.js
@@ -1,5 +1,18 @@
 function binarySearchPosition(numbers, target) {
-  // your code here
+  let start = 0;
+  let end = numbers.length - 1;
+  let mid = 0;
+  while (end >= start) {
+    mid = Math.floor((start + end) / 2)
+    if (target < numbers[mid]) {
+      end = mid;
+    } else if (target > numbers[mid]) {
+      start = mid;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
 }
 console.log(binarySearchPosition([1, 2, 5, 6, 7], 1)); // should print 0
 console.log(binarySearchPosition([1, 2, 5, 6, 7], 6)); // should print 3
