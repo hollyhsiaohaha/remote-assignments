7ec155b145a0f059c8e6a793bd5223e3430b64fc
holly <yahanhsiao@gmail.com>
2023-09-04 14:20:34 +0800

    add basic structure for w2

---
+++ Week-2/Assignment-5/w2a5.js
@@ -0,0 +1,5 @@
+function binarySearchPosition(numbers, target) {
+  // your code here
+}
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 1)); // should print 0
+console.log(binarySearchPosition([1, 2, 5, 6, 7], 6)); // should print 3
