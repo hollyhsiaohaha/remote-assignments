2ba9f1ac9289b3940c7179b3d00cd8d880a5de47
holly <yahanhsiao@gmail.com>
2023-09-11 14:19:24 +0800

    feat(w3a2): Add route daata
    http://localhost:3000/data will handle an interger query N, and send the sum  of series(1, 2, ... N) as result.

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extend: false }));
+
 app.get('/', (req, res) => {
-  res.send('aaaa');
+  res.send('Week3 assignment api');
+});
+
+app.get('/data', (req, res) => {
+  const { number } = req.query;
+  if (!number) {
+    res.send('Lack of Parameter');
+  }
+  if (!(Number(number))) {
+    res.send('Wrong Parameter');
+  }
+  const num = Number(number);
+  if (!Number.isInteger(num)) {
+    res.send('Wrong Parameter(should be interger)');
+  } else {
+    const sum = ((num + 1) * num) / 2;
+    res.send(`${sum}`);
+  }
 });
 
 app.listen(port, () => {
