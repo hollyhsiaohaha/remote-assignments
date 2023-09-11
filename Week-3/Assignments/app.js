2ceb413f131b80f1c8e7e55383b9c6f90dc1def8
holly <yahanhsiao@gmail.com>
2023-09-11 18:28:57 +0800

    feat(w3a4): Add myName and trackName api
    myName will show username if it was stored in cookie.
    if not, it will call trackName api via AJAX to store it into cookie, then redirect to myName again.

--- Week-3/Assignments/app.js
+++ Week-3/Assignments/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const bodyParser = require('body-parser');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+app.engine('html', require('ejs').renderFile);
+
 app.use(bodyParser.urlencoded({ extend: false }));
+app.use(cookieParser());
 app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.send('Week3 assignment api'));
 
@@ -24,6 +32,19 @@ app.get('/data', (req, res) => {
   }
   const sum = ((num + 1) * num) / 2;
   return res.send(`${sum}`);
+});
+
+app.get('/myName', (req, res) => {
+  const { userName } = req.cookies;
+  if (userName) {
+    return res.send(`Your user name is: ${userName}`);
+  }
+  return res.render('trackName.html');
+});
+
+app.get('/trackName', (req, res) => {
+  res.cookie('userName', req.query.name);
+  return res.send('cookie created'); // 如果刪掉這行就會報錯？why
 });
 
 app.listen(port, () => {
