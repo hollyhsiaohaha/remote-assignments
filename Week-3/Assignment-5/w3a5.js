cfaee1a99ebccfee890e6d6bccda8b1eb3ece4e2
holly <yahanhsiao@gmail.com>
2023-09-11 12:28:11 +0800

    feat(w3a1): build express app
    Install npm and express to build the app, and shows HTML page in localhost:3000

---
+++ Week-3/Assignment-5/w3a5.js
@@ -0,0 +1,13 @@
+function twoSum(nums, target) {
+  // your code here
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
+
+/*
+For example:
+twoSum([2, 7, 11, 15], 9); Should returns:
+[0, 1]
+Because:
+nums[0]+nums[1] is 9
+*/
