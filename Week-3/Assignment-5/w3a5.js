96f9c251fef611f229114c5c9e3030dc15efae6e
holly <yahanhsiao@gmail.com>
2023-09-12 14:50:45 +0800

    feat(w3a5): Complete function twoSum
    solved twoSum in 2 different ways

--- Week-3/Assignment-5/w3a5.js
+++ Week-3/Assignment-5/w3a5.js
@@ -1,8 +1,30 @@
-function twoSum(nums, target) {
-  // your code here
+function twoSum(nums, target) { // O(n^2)
+  for (let i = 0; i < nums.length; i += 1) {
+    for (let j = i + 1; j < nums.length; j += 1) {
+      if (target === nums[i] + nums[j]) {
+        return [i, j];
+      }
+    }
+  }
+  return -1;
+}
+
+function twoSum2(nums, target) { // O(n)
+  const discovered = {};
+  for (let i = 0; i < nums.length; i += 1) {
+    const anotherNum = target - nums[i];
+    if (discovered[anotherNum] !== undefined) {
+      return [discovered[anotherNum], i];
+    }
+    discovered[nums[i]] = i;
+  }
+  return -1;
 }
 
 console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([11, 2, 15, 7], 9));
+console.log(twoSum2([2, 7, 11, 15], 9));
+console.log(twoSum2([11, 2, 15, 7], 9));
 
 /*
 For example:
